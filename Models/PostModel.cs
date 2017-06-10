e4f7bf01967247321ca28a70d56440d0144b5f6c
justin leggett <justin@codingrookie.com>
2017-06-10 11:06:59 -0500

    added dbid field so I could more easily edit/delete items without manipulating the mongo ObjectId

--- Models/PostModel.cs
+++ Models/PostModel.cs
@@ -1,8 +1,11 @@
 using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 public class PostModel
 {
     public ObjectId _id { get; set; }
+    [BsonRequired]
+    public string dbid {get; set;}
     public string Company { get; set; }
     public string Posts { get; set; }
 	public string Username { get; set; }
--- dump.js
+++ dump.js
@@ -1,4 +1,5 @@
 db.posts.save({
+    "dbid": "89425uthgeohg89o5engeoln",
     "Username" : "justin",
     "Password" : "password",
     "Posts" : "This is my first post!",
@@ -8,6 +9,7 @@ db.posts.save({
 
 db.posts.save(
 {
+    "dbid": "fnrslkghajkrngjkdangdjlk",
     "Username" : "katie",
     "Password" : "password",
     "Posts" : "This is my first post and I made it way to long to see if I could break this app!",
@@ -16,6 +18,7 @@ db.posts.save(
 
 db.posts.save(
     {
+        "dbid": "fgjorslngdjkashfgoisrtjhlsdn",
         "Username" : "katie",
         "Password" : "password",
         "Posts" : "This is my first post and I made it way to long to see if I could break this app!",
