cd8e37c6f2d9c14a191ae909dc5e64581059eb13
justin leggett <justin@codingrookie.com>
2017-06-13 10:32:24 -0500

    trying to rename the project

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 
-namespace thechurchapp.api
+namespace thesocialapp.api
 {
     public class Program
     {
--- Startup.cs
+++ Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace thechurchapp.api
+namespace thesocialapp.api
 {
     public class Startup
     {
