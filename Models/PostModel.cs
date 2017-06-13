14387be61f5916ce26cd6ea97d124aac69024974
justin leggett <justin@codingrookie.com>
2017-06-12 21:00:49 -0500

    post checks for a dbid and rejects this request witout one

--- Models/PostModel.cs
+++ Models/PostModel.cs
@@ -1,10 +1,11 @@
+using System.ComponentModel.DataAnnotations;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
 public class PostModel
 {
     public ObjectId _id { get; set; }
-    [BsonRequired]
+    [Required]
     public string dbid {get; set;}
     public string Company { get; set; }
     public string Posts { get; set; }
